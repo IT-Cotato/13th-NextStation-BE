d1fe2d0fb01ea012da4f345b669e0135c5b249ed
Haewon Lee <ttoodong@naver.com>
2026-07-04 16:18:54 +0900

    chore: 프로젝트 초기 디렉토리 구조 세팅

---
+++ src/main/java/com/cotato/nextstation/NextstationApplication.java
@@ -0,0 +1,13 @@
+package com.cotato.nextstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextstationApplication.class, args);
+	}
+
+}
