f39d2d0ecf9cb366186a27da8c93bcd51625fba1
Haewon Lee <ttoodong@naver.com>
2026-07-04 16:32:51 +0900

    feat: swagger 라이브러리 설정 추가

--- src/main/java/com/cotato/nextstation/NextstationApplication.java
+++ src/main/java/com/cotato/nextstation/NextstationApplication.java
@@ -2,12 +2,13 @@ package com.cotato.nextstation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//TODO: swagger 정상 동작 확인 후 @SpringBootApplication로 수정
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class NextstationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NextstationApplication.class, args);
 	}
-
 }
