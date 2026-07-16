085de270f3b5ee3671e4e1204b4cce426ded390f
Haewon Lee <ttoodong@naver.com>
2026-07-16 23:40:33 +0900

    fix: DataSource 자동 설정 제외 임시 조치 제거

--- src/main/java/com/cotato/nextstation/NextstationApplication.java
+++ src/main/java/com/cotato/nextstation/NextstationApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-//TODO: swagger 정상 동작 확인 후 @SpringBootApplication로 수정
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class NextstationApplication {
 
 	public static void main(String[] args) {
