f3ac3df9914d596cb5f8d9ecd973330d15be8318
Haewon Lee <ttoodong@naver.com>
2026-07-24 01:52:35 +0900

    feat: place_tag_mapping을 태그명 기준으로 삭제하는 쿼리 추가

--- src/main/java/com/cotato/nextstation/domain/place/repository/PlaceTagMappingRepository.java
+++ src/main/java/com/cotato/nextstation/domain/place/repository/PlaceTagMappingRepository.java
@@ -4,6 +4,9 @@ import com.cotato.nextstation.domain.place.entity.Place;
 import com.cotato.nextstation.domain.place.entity.PlaceTagMapping;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PlaceTagMappingRepository extends JpaRepository<PlaceTagMapping, Long> {
@@ -11,4 +14,9 @@ public interface PlaceTagMappingRepository extends JpaRepository<PlaceTagMapping
     // 장소 상세 조회 - 이 장소의 태그 전부 조회 (표시용)
     @EntityGraph(attributePaths = {"placeTag"})
     List<PlaceTagMapping> findByPlace(Place place);
+
+    // 폐지된 place_tag 정리용 - enum에서 제거된 태그명도 지울 수 있게 문자열 기준으로 삭제한다
+    @Modifying
+    @Query(value = "DELETE ptm FROM place_tag_mapping ptm JOIN place_tag pt ON pt.id = ptm.place_tag_id WHERE pt.name IN (:tagNames)", nativeQuery = true)
+    void deleteByPlaceTagNames(@Param("tagNames") List<String> tagNames);
 }
