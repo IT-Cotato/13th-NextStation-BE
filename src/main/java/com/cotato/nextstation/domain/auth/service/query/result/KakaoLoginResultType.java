24a18777061f621b3ac9a1a117ca0e5efba59415
Haewon Lee <ttoodong@naver.com>
2026-07-27 00:29:07 +0900

    feat: 카카오 로그인/신규가입 판별 서비스 구현

---
+++ src/main/java/com/cotato/nextstation/domain/auth/service/query/result/KakaoLoginResultType.java
@@ -0,0 +1,7 @@
+package com.cotato.nextstation.domain.auth.service.query.result;
+
+public enum KakaoLoginResultType {
+    NEW_MEMBER,      // Member 미생성, kakaoSignupToken만 발급됨
+    PENDING_PROFILE, // Member(PENDING) 존재, signupToken 재발급됨
+    LOGIN_SUCCESS    // Member(ACTIVE), access+refresh 발급됨
+}
