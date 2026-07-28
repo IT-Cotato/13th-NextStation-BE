5864ff321e680247874b24a723a2d85387edf661
Cuyqo <saon0410@gmail.com>
2026-07-28 21:51:09 +0900

    feat: 클래스 선언부에 @Validated 추가

--- src/main/java/com/cotato/nextstation/domain/image/controller/ImageController.java
+++ src/main/java/com/cotato/nextstation/domain/image/controller/ImageController.java
@@ -14,12 +14,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/api/v1/images")
 public class ImageController {
 
