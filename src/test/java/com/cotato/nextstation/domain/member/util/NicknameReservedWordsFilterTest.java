28472e73164f0d421341d6fe84e7038d31a921ab
Haewon Lee <ttoodong@naver.com>
2026-07-30 19:53:07 +0900

    test: 닉네임 예약어 검증 단위 테스트 작성

---
+++ src/test/java/com/cotato/nextstation/domain/member/util/NicknameReservedWordsFilterTest.java
@@ -0,0 +1,40 @@
+package com.cotato.nextstation.domain.member.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NicknameReservedWordsFilterTest {
+
+    private final NicknameReservedWordsFilter filter = new NicknameReservedWordsFilter();
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "운영자", "최고운영자", "운영자1", "관리자", "시스템관리자",
+            "admin", "ADMIN", "Administrator", "System", "official"
+    })
+    @DisplayName("사칭 키워드가 부분 포함된 닉네임은 예약어로 판별된다")
+    void isReservedWord_containsBannedWords(String nickname) {
+        assertThat(filter.isReservedWord(nickname)).isTrue();
+    }
+
+    @Test
+    @DisplayName("브랜드명 명칭과 완전 일치하는 닉네임은 예약어로 판별된다")
+    void isReservedWord_exactBrandNameMatch() {
+        assertThat(filter.isReservedWord("환승여행")).isTrue();
+        assertThat(filter.isReservedWord("NEXTSTATION")).isTrue();
+        assertThat(filter.isReservedWord("환 승 여 행")).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "환승여행자", "환승여행러", "환승여행기", "일반유저", "길동이123"
+    })
+    @DisplayName("정상 닉네임 및 브랜드명이 부분 포함된 복합어 닉네임은 허용된다")
+    void isReservedWord_allowedNicknames(String nickname) {
+        assertThat(filter.isReservedWord(nickname)).isFalse();
+    }
+}
