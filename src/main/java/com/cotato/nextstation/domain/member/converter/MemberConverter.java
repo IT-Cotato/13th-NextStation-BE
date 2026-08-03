690bcdd9a2baca5929dcf884d30a0deed41c636a
Cuyqo <saon0410@gmail.com>
2026-08-04 08:28:36 +0900

    feat: 다른 회원 프로필 변환/조회 로직 추가

--- src/main/java/com/cotato/nextstation/domain/member/converter/MemberConverter.java
+++ src/main/java/com/cotato/nextstation/domain/member/converter/MemberConverter.java
@@ -1,6 +1,7 @@
 package com.cotato.nextstation.domain.member.converter;
 
 import com.cotato.nextstation.domain.member.dto.response.MemberProfileResponse;
+import com.cotato.nextstation.domain.member.dto.response.OtherMemberProfileResponse;
 import com.cotato.nextstation.domain.member.entity.Member;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +10,10 @@ public class MemberConverter {
 
     public MemberProfileResponse toProfileResponse(Member member) {
         return new MemberProfileResponse(member.getId(), member.getNickname(), member.getProfileImageUrl());
+    }
+
+    public OtherMemberProfileResponse toOtherProfileResponse(Member member, long stampCount, long publicCourseCount) {
+        return new OtherMemberProfileResponse(
+                member.getId(), member.getNickname(), member.getProfileImageUrl(), stampCount, publicCourseCount);
     }
 }
