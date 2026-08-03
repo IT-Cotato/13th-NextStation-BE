84d498388e6f365093d39bb46c0048ffb9b8f4e6
Cuyqo <saon0410@gmail.com>
2026-08-03 11:16:08 +0900

    feat: 내 스탬프 목록 조회 쿼리 추가

--- src/main/java/com/cotato/nextstation/domain/stamp/repository/MemberStampRepository.java
+++ src/main/java/com/cotato/nextstation/domain/stamp/repository/MemberStampRepository.java
@@ -1,6 +1,7 @@
 package com.cotato.nextstation.domain.stamp.repository;
 
 import com.cotato.nextstation.domain.stamp.entity.MemberStamp;
+import com.cotato.nextstation.domain.station.entity.LineCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -35,5 +36,25 @@ public interface MemberStampRepository extends JpaRepository<MemberStamp, Long> 
     // 완료된 스탬프 제외하고 미작성 스탬프만 조회 (최신순)
     List<MemberStamp> findByMemberIdAndIdNotInOrderByCreatedAtDesc(
             Long memberId, Set<Long> completedStampIds);
+
+    // 내 스탬프 목록 조회. 역별 중복 제거 시 최초 획득분을 남겨야 해서 오름차순으로 조회하고,
+    // 카드에 필요한 역/대표 호선까지 한 번에 가져온다(스탬프마다 조회하면 N+1).
+    @Query("SELECT s.id AS stationId, s.stationName AS stationName, " +
+            "l.id AS lineId, l.name AS lineName, l.code AS lineCode " +
+            "FROM MemberStamp ms " +
+            "JOIN Course c ON c.id = ms.courseId " +
+            "JOIN Station s ON s.id = c.stationId " +
+            "LEFT JOIN s.drawLine l " +
+            "WHERE ms.memberId = :memberId " +
+            "ORDER BY ms.createdAt ASC")
+    List<MyStampView> findMyStampsByMemberId(@Param("memberId") Long memberId);
+
+    interface MyStampView {
+        Long getStationId();
+        String getStationName();
+        Long getLineId();
+        String getLineName();
+        LineCode getLineCode();
+    }
 
 }
